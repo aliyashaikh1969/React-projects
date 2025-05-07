cc89641c876ddf28c43634b098266c40efc0735c
aliyashaikh1969 <aliyashaikh1969@gmail.com>
2025-05-07 16:42:26 +0530

    star rating

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 // import './App.css'
 // import { Accordion } from './component/accordian/Index'
 import RandomColorGenerator from "./component/random-color/App";
+import { StarRating } from "./component/star-rating/App";
 
 function App() {
   return (
     <>
       {/* <Accordion/> */}
 
-      <RandomColorGenerator />
+      {/* <RandomColorGenerator /> */}
+      <StarRating numberOfStar={5}/>
     </>
   );
 }
